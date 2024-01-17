1e4497282cbb31ee785692a2061363fa6baffde3
Huda Alam <hudalam03@gmail.com>
2024-01-17 20:48:15 +0530

    init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
-  
+  const [user,setUser]=useState(true)
   const logout =()=>{
+    user=false
     localStorage.clear()
   }
   return (
     <div className='navbar'>
       <span className='logo'><Link className="link" to="/">Huda's App</Link></span>
         <ul className="list">
-          <li className="listItem">
+          {user?<li className="listItem">
               <Link className='link' to="/login" onClick={logout}>Logout</Link>
-          </li>
+          </li>:
+          <li className="listItem">
+            <Link className='link' to="/login">Login</Link>
+        </li>}
+          
         </ul>
     </div>
   )
